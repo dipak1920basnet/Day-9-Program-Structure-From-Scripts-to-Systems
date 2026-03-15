0800763ed8bb29d83ce84aefb59c929b2ac1bbe7
dipak1920basnet <dipak1920basnet@gmail.com>
2026-03-15 18:18:29 +0545

    modular calculator

---
+++ simple_calculator/display.py
@@ -0,0 +1,3 @@
+def show_result(result):
+    print("Result: ", result)
+
---
+++ simple_calculator/main.py
@@ -0,0 +1,9 @@
+from operations import add
+from display import show_result
+
+def main():
+    result = add(5,3)
+    show_result(result)
+
+if __name__ == "__main__":
+    main()
